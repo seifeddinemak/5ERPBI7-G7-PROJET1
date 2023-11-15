39ad912ebc42b82e0d1906eddf4e9846c8b91fd2
mohamed123-alt <61463141+mohamed123-alt@users.noreply.github.com>
2023-11-15 22:44:54 +0100

    Update IRegistrationServices.java

--- src/main/java/tn/esprit/spring/services/IRegistrationServices.java
+++ src/main/java/tn/esprit/spring/services/IRegistrationServices.java
@@ -5,10 +5,10 @@ import tn.esprit.spring.entities.*;
 import java.util.List;
 
 public interface IRegistrationServices {
-
+/*
 	Registration addRegistrationAndAssignToSkier(Registration registration, Long numSkier);
 	Registration assignRegistrationToCourse(Long numRegistration, Long numCourse);
 	Registration addRegistrationAndAssignToSkierAndCourse(Registration registration, Long numSkieur, Long numCours);
-	List<Integer> numWeeksCourseOfInstructorBySupport(Long numInstructor, Support support);
+	List<Integer> numWeeksCourseOfInstructorBySupport(Long numInstructor, Support support);*/
 }
 
