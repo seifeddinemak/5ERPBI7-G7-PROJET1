e6110b68571a187d54b257dfc488fdc74ae11c93
mohamed123-alt <61463141+mohamed123-alt@users.noreply.github.com>
2023-11-15 22:33:42 +0100

    Update SkierServicesImpl.java

--- src/main/java/tn/esprit/spring/services/SkierServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/SkierServicesImpl.java
@@ -31,20 +31,9 @@ public class SkierServicesImpl implements ISkierServices {
 
     @Override
     public Skier addSkier(Skier skier) {
-        switch (skier.getSubscription().getTypeSub()) {
-            case ANNUAL:
-                skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusYears(1));
-                break;
-            case SEMESTRIEL:
-                skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusMonths(6));
-                break;
-            case MONTHLY:
-                skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusMonths(1));
-                break;
-        }
         return skierRepository.save(skier);
     }
-
+/*
     @Override
     public Skier assignSkierToSubscription(Long numSkier, Long numSubscription) {
         Skier skier = skierRepository.findById(numSkier).orElse(null);
@@ -52,7 +41,7 @@ public class SkierServicesImpl implements ISkierServices {
         skier.setSubscription(subscription);
         return skierRepository.save(skier);
     }
-
+/*
     @Override
     public Skier addSkierAndAssignToCourse(Skier skier, Long numCourse) {
         Skier savedSkier = skierRepository.save(skier);
@@ -65,7 +54,7 @@ public class SkierServicesImpl implements ISkierServices {
         }
         return savedSkier;
     }
-
+*/
     @Override
     public void removeSkier(Long numSkier) {
         skierRepository.deleteById(numSkier);
@@ -75,7 +64,7 @@ public class SkierServicesImpl implements ISkierServices {
     public Skier retrieveSkier(Long numSkier) {
         return skierRepository.findById(numSkier).orElse(null);
     }
-
+/*
     @Override
     public Skier assignSkierToPiste(Long numSkieur, Long numPiste) {
         Skier skier = skierRepository.findById(numSkieur).orElse(null);
@@ -94,5 +83,5 @@ public class SkierServicesImpl implements ISkierServices {
     @Override
     public List<Skier> retrieveSkiersBySubscriptionType(TypeSubscription typeSubscription) {
         return skierRepository.findBySubscription_TypeSub(typeSubscription);
-    }
+    }*/
 }
